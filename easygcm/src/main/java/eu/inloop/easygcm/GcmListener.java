5e44c7b3a63cb00f9b984fd5bc1d290d610ef9b3
Martin Adamek <adamek@inloop.eu>
2015-02-23 12:07:13 +0100

    Javadoc improvement

--- easygcm/src/main/java/eu/inloop/easygcm/GcmListener.java
+++ easygcm/src/main/java/eu/inloop/easygcm/GcmListener.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 
 public interface GcmListener {
 
+    /**
+     * Event called when message the is received.<br>
+     * It is <b>important to call</b> {@link WakeLockRelease#release()} when you are done handling the event
+     * to release the wake lock!
+     *
+     * @param messageType Message type as received from {@link com.google.android.gms.gcm.GoogleCloudMessaging#getMessageType(android.content.Intent)}
+     * @param extras content of the received intent
+     * @param wakeLockRelease helper for holding and releasing the wake lock. Always call {@link WakeLockRelease#release()} when you are done.
+     */
     void onMessage(String messageType, Bundle extras, WakeLockRelease wakeLockRelease);
 
     /**
-     * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP or CCS to send
-     * messages to your app. Not needed for this demo since the device sends upstream messages
-     * to a server that echoes back the message using the 'from' address in the message.
+     * Sends the registration ID to your server over HTTP.
+     * @param registrationId registration ID received from server
      */
     void sendRegistrationIdToBackend(String registrationId);
 
