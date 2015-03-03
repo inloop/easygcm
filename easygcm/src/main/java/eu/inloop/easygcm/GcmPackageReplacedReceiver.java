74740a4d231d44e68bb2fcf9715dda63b80578b5
David Vavra <me@destil.cz>
2015-03-03 18:03:37 +0100

    Support for setting custom GcmListener
    We would like to integrate your library in our library and we don’t
    have access to the Application instance. That’s why we can’t implement
    GcmListener directly. This commit adds method setGcmListener() which
    should be (optionally) called in Application.onCreate()

--- easygcm/src/main/java/eu/inloop/easygcm/GcmPackageReplacedReceiver.java
+++ easygcm/src/main/java/eu/inloop/easygcm/GcmPackageReplacedReceiver.java
@@ -9,20 +9,15 @@ public class GcmPackageReplacedReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, final Intent intent) {
         if (intent != null && intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
-            if (context.getApplicationContext() instanceof GcmListener) {
-                if (GcmHelper.sLoggingEnabled) {
-                    Log.d(GcmUtils.TAG, "Received application update broadcast");
-                }
-                GcmHelper.getInstance().registerInBackground(context, new GcmHelper.RegistrationListener() {
-                    @Override
-                    public void onFinish() {
-                        WakefulBroadcastReceiver.completeWakefulIntent(intent);
-                    }
-                });
-            } else {
-                WakefulBroadcastReceiver.completeWakefulIntent(intent);
-                throw new IllegalStateException("Application must implement GcmListener interface!");
+            if (GcmHelper.sLoggingEnabled) {
+                Log.d(GcmUtils.TAG, "Received application update broadcast");
             }
+            GcmHelper.getInstance().registerInBackground(context, new GcmHelper.RegistrationListener() {
+                @Override
+                public void onFinish() {
+                    WakefulBroadcastReceiver.completeWakefulIntent(intent);
+                }
+            });
         }
     }
 }
