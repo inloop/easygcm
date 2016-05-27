fea5065f0c9810d55f75e752727d20b53b07df0d
Markus <m.wedler@cosomedia.de>
2016-05-27 13:31:48 +0200

    fix ConnectivityManager context leak

--- easygcm-sample/src/main/java/eu/inloop/easygcm/sample/MainActivity.java
+++ easygcm-sample/src/main/java/eu/inloop/easygcm/sample/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         EasyGcm.setLoggingLevel(EasyGcm.Logger.LEVEL_WARNING);
-        EasyGcm.init(this);
+        EasyGcm.init(getApplicationContext());
     }
 
 }
